eaa17c11cf71df1a11fbd60756032c5e5136eb1d
Eric Lineback <eric@interopion.com>
2017-11-22 10:31:09 -0700

    Revert "    test to see if environments working"
    This reverts commit 954a9ad

--- src/main/java/org/hspconsortium/sandboxmanagerapi/services/impl/JwtServiceImpl.java
+++ src/main/java/org/hspconsortium/sandboxmanagerapi/services/impl/JwtServiceImpl.java
@@ -20,7 +20,6 @@ public class JwtServiceImpl implements JwtService {
 
     @Override
     public String createSignedJwt(String subject) {
-        System.out.println("The jwtkey: " + jwtKey.toString());
         return Jwts.builder()
                 .setSubject(subject)
                 .signWith(SignatureAlgorithm.forName(signatureAlgorithm), jwtKey)
