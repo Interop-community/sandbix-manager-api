c60f0d1109ed7a2eb6a5d0d7add58387dd22230d
Travis Cummings <travis@interopion.com>
2018-02-10 10:13:31 -0700

    New pipeline

--- ci/build_set_env.sh
+++ ci/build_set_env.sh
@@ -7,10 +7,12 @@ echo "starting $0..."
 [[ -z "$1" ]] && { echo "usage: $0 {test | prod} port memory(mb)"; exit 1; } || echo "ENV: $1"
 [[ -z "$2" ]] && { echo "usage: $0 {test | prod} port memory(mb)"; exit 1; } || echo "ENV: $2"
 [[ -z "$3" ]] && { echo "usage: $0 {test | prod} port memory(mb)"; exit 1; } || echo "ENV: $3"
+[[ -z "$4" ]] && { POM="pom.xml"; } || POM="${4}/pom.xml"
+echo "POM: $POM"
 
 export DOCKER_REPO="hspconsortium"; echo DOCKER_REPO
-export IMAGE_NAME=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.artifactId}' --non-recursive exec:exec); echo $IMAGE_NAME
-export PROJECT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec); echo $PROJECT_VERSION
+export IMAGE_NAME=$(mvn -q -f ${POM} -Dexec.executable="echo" -Dexec.args='${project.artifactId}' --non-recursive exec:exec); echo $IMAGE_NAME
+export PROJECT_VERSION=$(mvn -q -f ${POM} -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec); echo $PROJECT_VERSION
 export IMAGE_COORDINATES="${DOCKER_REPO}/${IMAGE_NAME}:${PROJECT_VERSION}"; echo $IMAGE_COORDINATES;
 export IMAGE_PORT="$2"; echo $IMAGE_PORT;
 export IMAGE_MEMORY_RESERVATION="$3"; echo $IMAGE_MEMORY_RESERVATION;
@@ -20,6 +22,7 @@ export AWS_CONTAINER_NAME=$IMAGE_NAME; echo $AWS_CONTAINER_NAME
 export AWS_SERVICE_NAME=$IMAGE_NAME; echo $AWS_SERVICE_NAME
 
 echo "building set_env.sh..."
+echo "#!/usr/bin/env bash" >> set_env.sh
 echo "export DOCKER_REPO=$DOCKER_REPO" >> set_env.sh
 echo "export IMAGE_NAME=$IMAGE_NAME" >> set_env.sh
 echo "export PROJECT_VERSION=$PROJECT_VERSION" >> set_env.sh
