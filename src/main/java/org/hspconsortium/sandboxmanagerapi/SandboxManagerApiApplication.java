aae335fbf65dd1135d1014c5e49347b874125c4f
Travis Cummings <travis@interopion.com>
2018-02-19 23:58:15 -0700

    cors

--- src/main/java/org/hspconsortium/sandboxmanagerapi/SandboxManagerApiApplication.java
+++ src/main/java/org/hspconsortium/sandboxmanagerapi/SandboxManagerApiApplication.java
@@ -2,7 +2,11 @@ package org.hspconsortium.sandboxmanagerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan({"org.hspconsortium"})
 @SpringBootApplication
@@ -12,4 +16,17 @@ public class SandboxManagerApiApplication {
         SpringApplication.run(SandboxManagerApiApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedHeaders("X-FHIR-Starter,authorization,Prefer,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers")
+                        .allowedMethods("POST, PUT, GET, OPTIONS, DELETE")
+                ;
+            }
+        };
+    }
 }
