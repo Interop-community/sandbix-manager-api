0a87dda0fe7243b21ae090669a5f9aa3ec6f32fe
Travis Cummings <travis@interopion.com>
2018-02-20 02:17:00 -0700

    cors

--- src/main/java/org/hspconsortium/sandboxmanagerapi/config/ResourceWebConfig.java
+++ src/main/java/org/hspconsortium/sandboxmanagerapi/config/ResourceWebConfig.java
@@ -1,10 +1,22 @@
 package org.hspconsortium.sandboxmanagerapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-//@ComponentScan({"org.hspconsortium"})
-public class ResourceWebConfig extends WebMvcConfigurerAdapter {}
+public class ResourceWebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
