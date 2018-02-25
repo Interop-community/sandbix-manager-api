e33a23407ea97e80cf6e1b9a995db6e8bcf3e022
Travis Cummings <travis@interopion.com>
2018-02-25 16:41:26 -0700

    new pipeline

---
+++ ci/ci-2-docker-image.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
+docker/build.sh $DOCKER_PROJECT_VERSION
+#docker build --build-arg ACTIVE_ENV=$ACTIVE_ENV -t $DOCKER_IMAGE_COORDINATES ..
+echo "docker push..."
+docker push "$DOCKER_IMAGE_COORDINATES"
