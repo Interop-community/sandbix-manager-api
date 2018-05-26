5db55142cc2ed414f3d46b9c4fa34358a2a6d3dc
Travis Cummings <travis@interopion.com>
2018-05-25 22:38:57 -0600

    Misc scripts

--- src/main/resources/db/mysql/delete_sandbox.sql
+++ src/main/resources/db/mysql/delete_sandbox.sql
@@ -6,4 +6,14 @@ DELETE FROM user_sandbox WHERE sandbox_id = @sandbox_id;
 
 DELETE FROM sandbox_activity_log WHERE sandbox_id = @sandbox_id;
 
-DELETE FROM sandbox WHERE sandbox_id = @sandbox_id;
+DELETE FROM launch_scenario WHERE sandbox_id = @sandbox_id;
+
+DELETE FROM patient WHERE sandbox_id = @sandbox_id;
+
+DELETE FROM app WHERE sandbox_id = @sandbox_id;
+
+DELETE FROM user_persona WHERE sandbox_id = @sandbox_id;
+
+DELETE FROM sandbox_invite WHERE sandbox_id = @sandbox_id;
+
+DELETE FROM sandbox WHERE id = @sandbox_id;
