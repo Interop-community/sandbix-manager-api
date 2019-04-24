daf54a48da71148e3ed17940af79b49633ba6d97
Jacob Crump <jacob@interopion.com>
2019-04-24 16:39:20 -0600

    One more thing to try.

--- src/main/java/org/hspconsortium/sandboxmanagerapi/services/impl/SandboxServiceImpl.java
+++ src/main/java/org/hspconsortium/sandboxmanagerapi/services/impl/SandboxServiceImpl.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
@@ -147,7 +148,7 @@ public class SandboxServiceImpl implements SandboxService {
     }
 
     @Inject
-    public void setFhirProfileDetailService(FhirProfileDetailService fhirProfileDetailService) {
+    public void setFhirProfileDetailService(@Lazy FhirProfileDetailService fhirProfileDetailService) {
         this.fhirProfileDetailService = fhirProfileDetailService;
     }
 
