788037d2f0d5d5e63b0bdd4a849118a31a915f1d
Shilpy <shilpy@interopion.com>
2019-07-17 09:27:59 -0600

    Added test for saving zip files, didn't work, still need to work on it.

--- src/test/java/org/hspconsortium/sandboxmanagerapi/services/FhirProfileDetailServiceTest.java
+++ src/test/java/org/hspconsortium/sandboxmanagerapi/services/FhirProfileDetailServiceTest.java
@@ -1,21 +1,26 @@
 package org.hspconsortium.sandboxmanagerapi.services;
 
+import org.apache.commons.io.FileUtils;
 import org.hspconsortium.sandboxmanagerapi.model.*;
 import org.hspconsortium.sandboxmanagerapi.repositories.FhirProfileDetailRepository;
 import org.hspconsortium.sandboxmanagerapi.services.impl.FhirProfileDetailServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
+import org.junit.Rule;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -37,8 +42,11 @@ public class FhirProfileDetailServiceTest {
     private Sandbox sandbox;
     private User user;
 
+    @Rule
+    public TemporaryFolder tempFolder= new TemporaryFolder();
+
     @Before
-    public void setup() {
+    public void setup() throws Exception {
         String[] profileResources = new String[]{"StructureDefinition", "CodeSystem", "ValueSet", "SearchParameter"};
         ReflectionTestUtils.setField(fhirProfileDetailService, "profileResources", profileResources);
         fhirProfileDetailService.setFhirProfileService(fhirProfileService);
@@ -109,12 +117,63 @@ public class FhirProfileDetailServiceTest {
     }
 
     @Test
-    public void saveZipFileTest() {
-        InputStream inputStream = new ByteArrayInputStream("test data".getBytes());
+    public void saveZipFileTest() throws Exception {
+        File tempFile = tempFolder.newFile("tempFile.txt");
+        FileUtils.writeStringToFile(tempFile, "hello world");
+        ZipFile zipFile = new ZipFile(tempFile); //TODO: Can't zip the file
         when(sandboxService.findBySandboxId("sandbox")).thenReturn(sandbox);
         when(sandboxService.getApiSchemaURL("apiEndpointIndex")).thenReturn("localhost");
-        fhirProfileDetailService.saveZipFile(fhirProfileDetail, inputStream, anyString(), anyString(), anyString());
+        fhirProfileDetailService.saveZipFile(fhirProfileDetail, zipFile, anyString(), anyString(), anyString());
+        verify(sandboxService).findBySandboxId("1");
     }
 
+    @Test
+    public void saveTGZfile() throws Exception {
+        File file = new File(".txt"); //TODO: Add file here
+        InputStream fileInputStream = new FileInputStream(file);
+        fhirProfileDetailService.saveTGZfile(fhirProfileDetail, fileInputStream, "abcd", "1", "1");
+        verify(sandboxService).findBySandboxId("1");
+    }
+
+    public static void createZipFile () {
+        try {
+            FileOutputStream fos = new FileOutputStream("sample.zip");
+            ZipOutputStream zipOS = new ZipOutputStream(fos);
+
+            String file1 = "ValueSet.json";
+            String file2 = "SearchParameter.json";
+            String file3 = "StructureDefinition.json";
+            String file4 = "CodeSystem.json";
+
+            writeToZipFile(file1, zipOS);
+            writeToZipFile(file2, zipOS);
+            writeToZipFile(file3, zipOS);
+            writeToZipFile(file4, zipOS);
+
+            zipOS.close();
+            fos.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeToZipFile(String path, ZipOutputStream zipStream) throws FileNotFoundException, IOException {
+        File aFile = new File(path);
+        FileInputStream fis = new FileInputStream(aFile);
+        ZipEntry zipEntry = new ZipEntry(path);
+        zipStream.putNextEntry(zipEntry);
+
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zipStream.write(bytes, 0, length);
+        }
+
+        zipStream.closeEntry();
+        fis.close();
+    }
 
 }
