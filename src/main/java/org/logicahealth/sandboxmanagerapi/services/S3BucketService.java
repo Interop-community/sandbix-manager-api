50033b9ad26929361a4be09e34d11562825b4ef3
Gopal Menon <gopal@interopion.com>
2021-07-12 13:53:45 -0600

    Removed unused code

--- src/main/java/org/logicahealth/sandboxmanagerapi/services/S3BucketService.java
+++
@@ -1,9 +0,0 @@
-package org.logicahealth.sandboxmanagerapi.services;
-
-import java.io.InputStream;
-
-public interface S3BucketService {
-    void putFile(String fileName, InputStream inputStream);
-    void notifyUser(String user);
-    void cleanUpOldFiles();
-}
